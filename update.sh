4bff28cb30b05ab036b30c211d4f7f921774ef5c
indomiehaxor <bekasiblackhunter1@gmail.com>
2024-06-06 09:46:03 +0700

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,55 @@
+#
+#   _   _   ___  _____  _   _____________ _____ _   _ _____  ___   _   _ 
+#  | | | | / _ \/  __ \| | / /_   _| ___ \  _  | \ | |_   _|/ _ \ | \ | |
+#  | |_| |/ /_\ \ /  \/| |/ /  | | | |_/ / | | |  \| | | | / /_\ \|  \| |
+#  |  _  ||  _  | |    |    \  | | |    /| | | | . ` | | | |  _  || . ` |
+#  | | | || | | | \__/\| |\  \ | | | |\ \\ \_/ / |\  |_| |_| | | || |\  |
+#  \_| |_/\_| |_/\____/\_| \_/ \_/ \_| \_|\___/\_| \_/\___/\_| |_/\_| \_/
+#
+#                   ~ Tools For Hacking by indomie haxor ~
+
+clear
+
+sudo chmod +x /etc/
+
+clear
+
+sudo chmod +x /usr/share/doc
+
+clear
+
+sudo rm -rf /usr/share/doc/indomiehaxor/
+
+clear
+
+cd /etc/
+
+clear
+
+sudo rm -rf /etc/indomiehaxor
+
+clear
+
+mkdir indomiehaxor
+
+clear
+
+cd indomiehaxor
+
+clear
+
+git clone https://github.com/indomiehaxor/hacktronian.git
+
+clear
+
+cd hacktronian
+
+clear
+
+sudo chmod +x install.sh
+
+clear
+
+./install.sh
+
+clear
